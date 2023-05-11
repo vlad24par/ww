2965f9d688634d3f614c1fc86674dd68c734cffc
Yana <talioo1997@gmail.com>
2023-05-11 21:12:15 +0300

    Improve spawn field

--- Assets/scripts/FieldSettings.cs
+++ Assets/scripts/FieldSettings.cs
@@ -7,4 +7,11 @@ public class FieldSettings : ScriptableObject
 {
     public int X;
     public int Y;
+
+    [Space(10)]
+    public int HillPercent = 0;
+    public int MountainPercent = 0;
+    public int ForestPercent = 0;
+    public int FieldPercent = 0;
+    public int SwampPercent = 0;
 }
--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -5,52 +5,116 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private List<Sell> _sellsPrefabs;
+    [SerializeField] private List<FieldSettings> _fieldSettingsList;
 
     [SerializeField] private int X;
     [SerializeField] private int Y;
-    [SerializeField] float Given_Index;
-    private float Bioms_indecks;
-    private float Bioms_indecks2;
+    [SerializeField] uint Given_Index;
+    
     private int Number_Of_Biomes = 10000;
 
     void Start()
     {
-        BuildField();
+        BuildFieldBySettings();
     }
 
     private void BuildField()
     {
+        float Bioms_indecks;
+        float Bioms_indecks2;
+        
         for (int i = 0; i < X; i++)
         {
             for (int j = 0; j < Y; j++)
             {
                 Bioms_indecks = Given_Index;
                 Bioms_indecks2 = Given_Index;
-                Number_Of_Biomes = Random.Range(1, 5);
-                if (Number_Of_Biomes == 1)
+                Number_Of_Biomes = Random.Range(0, 5);
+                
+                switch (Number_Of_Biomes)
                 {
-                    Number_Of_Biomes = 10;
+                    case 1: 
+                        Number_Of_Biomes = 1;
+                        break;
+                    case 2: 
+                        Number_Of_Biomes = 10;
+                        break;
+                    case 3: 
+                        Number_Of_Biomes = 100;
+                        break;
+                    case 4: 
+                        Number_Of_Biomes = 1000;
+                        break;
+                    case 5: 
+                        Number_Of_Biomes = 10000;
+                        break;
+                    
+                    default:
+                        Number_Of_Biomes = 11111;
+                        break;
                 }
-                else if (Number_Of_Biomes == 2)
-                {
-                    Number_Of_Biomes = 100;
-                }
-                else if (Number_Of_Biomes == 3)
-                {
-                    Number_Of_Biomes = 1000;
-                }
-                else if (Number_Of_Biomes == 4)
-                {
-                    Number_Of_Biomes = 10000;
-                }
-                Bioms_indecks = Bioms_indecks / Number_Of_Biomes;
+                
+                Bioms_indecks /= Number_Of_Biomes;
                 Bioms_indecks = Mathf.Floor(Bioms_indecks);
-                Bioms_indecks2 = Bioms_indecks2 / (Number_Of_Biomes / 10);
+                
+                Bioms_indecks2 /= (Number_Of_Biomes / 10);
                 Bioms_indecks2 = Mathf.Floor(Bioms_indecks2);
+                
                 Bioms_indecks = Bioms_indecks2 - Bioms_indecks * 10;
                 var prefab = _sellsPrefabs[(int)Bioms_indecks];
+                
                 Instantiate(prefab, new Vector3(i, j, 0), Quaternion.identity,transform);
             }
         }
+    }
+
+    private void BuildFieldBySettings()
+    {
+        var fieldId = Random.Range(0, _fieldSettingsList.Count);
+        var settings = _fieldSettingsList[fieldId];
+        
+        for (int i = 0; i < X; i++)
+        {
+            for (int j = 0; j < Y; j++)
+            {
+                var value = Random.Range(0, 101);
+                if (value <= settings.HillPercent)
+                {
+                    SpawnSell(_sellsPrefabs[0].gameObject, new Vector3(i, j, 0));
+                    continue;
+                }
+
+                if (value <= settings.HillPercent + 
+                    settings.MountainPercent)
+                {
+                    SpawnSell(_sellsPrefabs[1].gameObject, new Vector3(i, j, 0));
+                    continue;
+                }
+
+                if (value <= settings.HillPercent + 
+                    settings.MountainPercent + 
+                    settings.ForestPercent)
+                {
+                    SpawnSell(_sellsPrefabs[2].gameObject, new Vector3(i, j, 0));
+                    continue;
+                }
+
+                if (value <= settings.HillPercent + 
+                    settings.MountainPercent+ 
+                    settings.ForestPercent + 
+                    settings.FieldPercent)
+                {
+                    SpawnSell(_sellsPrefabs[3].gameObject, new Vector3(i, j, 0));
+                    continue;
+                }
+                
+                SpawnSell(_sellsPrefabs[4].gameObject, new Vector3(i, j, 0));
+            }
+        }
+    }
+
+    private void SpawnSell(GameObject prefab, Vector3 position)
+    {
+        Instantiate(prefab, position, Quaternion.identity,transform);
     }
 }
